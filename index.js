cb5e849c17bf62006d05f948ce60ec5b548914a2
Wakype <hilmim152bisnis@gmail.com>
2023-01-20 13:24:54 +0700

    [20/01/2023] Database Check

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 // const fs = require('fs-extra')
 const multer = require('multer');
 const dotenv = require('dotenv').config();
-
+const { sequelize } = require('./src/models');
 
 const routers = require('./src/routes/routers');
 const log = require('./src/middleware/log');
@@ -11,14 +11,13 @@ const notFound = require('./src/middleware/404');
 const errorHandling = require('./src/middleware/errorHandling');
 const auth = require('./src/middleware/auth');
 
-
 const app = express();
 const port = process.env.PORT || 8081;
 
 // app.use(bodyParser.json());
-app.use(express.json())
+app.use(express.json());
 app.use(log);
-app.use(express.static('./src/storage/uploads'))
+app.use(express.static('./src/storage/uploads'));
 // app.use(auth);
 app.use(routers);
 app.use(notFound);
@@ -29,6 +28,12 @@ app.use(notFound);
 //   console.log(`Server berjalan di http://localhost:${port}`)
 // );
 
-app.listen(port, function () {
-  return console.log(`🖥️  ➤➤➤ Server berjalan di http://localhost:${port}`);
+app.listen(port, async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('🚧 ➤➤➤ Connection has been established successfully.');
+    console.log(`🖥️  ➤➤➤ Server berjalan di http://localhost:${port}`);
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
 });
