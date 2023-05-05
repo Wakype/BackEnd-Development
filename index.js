906261ba285a82cd37dcdf605d3523c044d36c15
Wakype <hilmim152bisnis@gmail.com>
2023-05-05 08:21:52 +0700

    first commit

--- index.js
+++ index.js
@@ -1,38 +1,68 @@
+const dotenv = require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-// const fs = require('fs-extra')
 const multer = require('multer');
-const dotenv = require('dotenv').config();
+const http = require('http');
+const cors = require('cors');
+
+
 const { sequelize } = require('./src/models');
+const { Server } = require('socket.io');
 
 const routers = require('./src/routes/routers');
 const log = require('./src/middleware/log');
 const notFound = require('./src/middleware/404');
 const errorHandling = require('./src/middleware/errorHandling');
-const auth = require('./src/middleware/auth');
 const paginationMiddleware = require('./src/middleware/pageSizeMiddleware');
 
 const app = express();
 const port = process.env.PORT || 8081;
 
-// app.use(bodyParser.json());
 app.use(express.json());
 app.use(log);
+app.use(cors())
 app.use(express.static('./src/storage/uploads'));
 app.use(paginationMiddleware);
-// app.use(auth);
 app.use(routers);
 app.use(notFound);
 app.use(errorHandling);
-app.use(notFound);
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log('connected with socked id =>', socket.id);
+
+  // code here
+  socket.on('join_room', (data) => {
+    socket.join(data.roomCode);
+    console.log('kode room: ', data);
+  });
+  socket.on('send_message', (data) => {
+    console.log('send_message data', data)
+    socket.to(data.room).emit("received_message", data)
+  })
+  socket.on('broadcast', (data) => {
+    console.log('pesan broadcast =>', data)
+    socket.broadcast.emit('broadcast_received', data)
+  })
+  socket.on('disconnect', () => {
+    console.log('user disconnected', socket.id);
+  });
+});
 
 // app.listen(port, () =>
 //   console.log(`Server berjalan di http://localhost:${port}`)
 // );
 
-app.listen(port, async () => {
+server.listen(port, async () => {
   try {
-    await sequelize.authenticate();
+    // await sequelize.authenticate();
     console.log('🚧 ➤➤➤ Connection has been established successfully.');
     console.log(`🖥️  ➤➤➤ Server berjalan di http://localhost:${port}`);
   } catch (error) {
